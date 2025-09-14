00155ed0ffa3ace26f40db94dac9544736d72560
Michael Anggi Gilang Angkasa <63223847+Michael-Obs66@users.noreply.github.com>
2025-09-14 10:43:00 +0700

    Update Variable and Basic Operations.sh

--- Fundamental/Variable and Basic Operations.sh
+++ Fundamental/Variable and Basic Operations.sh
@@ -1,1 +1,25 @@
+#!/bin/bash
+
+#variable declaration
+target_ip="192.168.1.100" #change the target for what you want
+port=80
+scan_type="TCP"
+
+#String Concatenation
+full_target="${target_ip}:${port}"
+log_message="Scanning ${full_target}:Using ${scan_type}"
+
+#numeric Operation
+total_port=$((port + 21 + 443 + 8080))
+echo "Total Ports to Scan : $total_port"
+
+#arrays for multiple target
+targets= "192.168.1.100" "192.168.1.182" "192.168.1.183"
+ports= ( 21 22 23 25 53 80 110 443 993 995 )
+
+echo "Targets : ${targets[@]}"
+echo "Ports : ${ports[@]}"
+echo "Total Targets : ${#targets[@]}"
+
+
 
